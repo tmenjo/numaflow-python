00cd5169033a8ce4a7c3e2a795be9f003861dec5
Takashi Menjo <takashi.menjo+github@gmail.com>
2026-05-19 11:05:29 +0900

    chore: streamsorter is now blackhole
    Signed-off-by: Takashi Menjo <takashi.menjo+github@gmail.com>

--- packages/pynumaflow/examples/accumulator/streamsorter/example.py
+++ packages/pynumaflow/examples/accumulator/streamsorter/example.py
@@ -35,43 +35,6 @@ class StreamSorter(Accumulator):
                 f"Datum watermark: {datum.watermark}"
             )
 
-            # If watermark has moved forward
-            if datum.watermark and datum.watermark > self.latest_wm:
-                self.latest_wm = datum.watermark
-                _LOGGER.info(f"Watermark updated: {self.latest_wm}")
-                await self.flush_buffer(output)
-
-            self.insert_sorted(datum)
-
-        _LOGGER.info("Timeout reached")
-        await self.flush_buffer(output, flush_all=True)
-
-    def insert_sorted(self, datum: Datum):
-        # Binary insert to keep sorted buffer in order
-        left, right = 0, len(self.sorted_buffer)
-        while left < right:
-            mid = (left + right) // 2
-            if self.sorted_buffer[mid].event_time > datum.event_time:
-                right = mid
-            else:
-                left = mid + 1
-        self.sorted_buffer.insert(left, datum)
-
-    async def flush_buffer(self, output: NonBlockingIterator, flush_all: bool = False):
-        if flush_all:
-            _LOGGER.info("Flushing entire sortedBuffer")
-        else:
-            _LOGGER.info(f"Flushing sortedBuffer above watermark: {self.latest_wm}")
-        i = 0
-        for datum in self.sorted_buffer:
-            if datum.event_time > self.latest_wm and not flush_all:
-                break
-            await output.put(Message.from_datum(datum))
-            _LOGGER.info(f"Sent datum with event time: {datum.event_time}")
-            i += 1
-        # Remove flushed items
-        self.sorted_buffer = self.sorted_buffer[i:]
-
 
 if __name__ == "__main__":
     grpc_server = None
